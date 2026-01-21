86a49ce42c89f007369c4a743be18591804631c9
hzrd149 <github@hzrd149.com>
2026-01-20 19:47:43 -0600

    Fix `User` chainable observables not synchronously emitting values

---
+++ .changeset/wise-hornets-relax.md
@@ -0,0 +1,5 @@
+---
+"applesauce-common": patch
+---
+
+Fix `User` chainable observables not synchronously emitting values
--- packages/common/src/casts/__tests__/user.test.ts
+++ packages/common/src/casts/__tests__/user.test.ts
@@ -2,15 +2,21 @@ import { EventStore } from "applesauce-core/event-store";
 import { describe, expect, it } from "vitest";
 import { FakeUser } from "../../__tests__/fixtures";
 import { castUser } from "../user";
+import { firstValueFrom } from "rxjs";
 
 describe("user", () => {
   describe("references", () => {
-    it("should support sync observable properties", () => {
+    it("should support sync observable properties", async () => {
       const signer = new FakeUser();
       const profile = signer.profile({ name: "John Doe" });
       const eventStore = new EventStore();
       eventStore.add(profile);
+
       const user = castUser(profile, eventStore);
+
+      // Subscribe once to load the circular dependency
+      await firstValueFrom(user.profile$.name);
+
       const chain = user.profile$.name;
 
       let name: string | undefined = "";
